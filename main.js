cd2f4bee7ad9a6caf433d9891de69ef0e89a33f6
twogog <you@example.com>
2023-07-27 15:47:24 +0600

    testing serverless

--- main.js
+++ main.js
@@ -1,24 +1,30 @@
-(async () => {
-  require("dotenv").config();
-  const { Telegraf } = require("telegraf");
-  const { message } = require("telegraf/filters");
-  const { getCurrency } = require("./currency");
+require("dotenv").config();
+const { Telegraf } = require("telegraf");
+const { message } = require("telegraf/filters");
+const { getCurrency } = require("./currency");
 
-  const bot = new Telegraf(process.env.TOKEN);
+const bot = new Telegraf(process.env.TOKEN);
 
-  bot.start((ctx) => ctx.reply("Привет, выбери команду из главного меню)"));
+bot.start((ctx) => ctx.reply("Привет, выбери команду из главного меню)"));
 
-  bot.command("currency", async (ctx) => {
-    getCurrency(ctx);
+bot.command("currency", async (ctx) => {
+  getCurrency(ctx);
+});
+
+if (process.env.environment == "PRODUCTION") {
+  bot.launch({
+    webhook: {
+      domain: process.env.DOMAIN, // Your domain URL (where server code will be deployed)
+      port: process.env.PORT || 8000,
+    },
   });
+} else {
+  // if local use Long-polling
+  bot.launch().then(() => {
+    console.info(`The bot ${bot.botInfo.username} is running locally`);
+  });
+}
 
-  // bot.on(message("text"), (ctx) => {
-  //   console.log(ctx.message);
-  // });
-
-  bot.launch();
-
-  // Enable graceful stop
-  process.once("SIGINT", () => bot.stop("SIGINT"));
-  process.once("SIGTERM", () => bot.stop("SIGTERM"));
-})();
+// Enable graceful stop
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
