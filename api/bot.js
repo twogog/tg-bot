249bedc9354e8ab41bfa47d0757fd142ddbb57f9
twogog <you@example.com>
2023-07-27 16:29:24 +0600

    testing vercel

--- api/bot.js
+++ api/bot.js
@@ -1,3 +1,42 @@
-export default function handler(request, response) {
-  response.status(200).json("dude");
-}
+module.exports = async (request, response) => {
+  try {
+    process.env.NTBA_FIX_319 = "test";
+
+    const { Telegraf } = require("telegraf");
+    const { message } = require("telegraf/filters");
+    const { getCurrency } = require("../currency");
+
+    const bot = new Telegraf(process.env.TOKEN);
+
+    // Retrieve the POST request body that gets sent from Telegram
+    const { body } = request;
+
+    // Ensure that this is a message being sent
+    if (body.message) {
+      // Retrieve the ID for this chat
+      // and the text that the user sent
+      const {
+        chat: { id },
+        text,
+      } = body.message;
+
+      // Create a message to send back
+      // We can use Markdown inside this
+      const message = `✅ Thanks for your message: *"${text}"*\nHave a great day! 👋🏻`;
+
+      // Send our new message back in Markdown and
+      // wait for the request to finish
+      await bot.sendMessage(id, message, { parse_mode: "Markdown" });
+    }
+  } catch (error) {
+    // If there was an error sending our message then we
+    // can log it into the Vercel console
+    console.error("Error sending message");
+    console.log(error.toString());
+  }
+
+  // Acknowledge the message with Telegram
+  // by sending a 200 HTTP status code
+  // The message here doesn't matter.
+  response.send("OK");
+};
