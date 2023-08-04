ad25583a2e7babee45f55b3f33dce318cd26bea7
twogog <you@example.com>
2023-08-04 22:57:00 +0600

    fixed curr message

--- currency.js
+++ currency.js
@@ -4,13 +4,15 @@ async function getCurrency(ctx) {
   const response = await fetch(url);
   const data = await response.json();
   const { Valute } = data;
+  let message = [];
   Object.entries(Valute)
     .filter((v) => ["EUR", "USD"].includes(v[0]))
     .forEach(async (ar) => {
       const [name, info] = ar;
       const { Name, Previous, Value } = info;
-      await ctx.reply(`${Name}: предыдущий - ${Previous}; нынешний - ${Value}`);
+      message.push(`${Name}: предыдущий - ${Previous}; нынешний - ${Value}`);
     });
+  await ctx.reply(message.join("\n"));
 }
 
 module.exports = {
